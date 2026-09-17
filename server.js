e1744ac429fddcd1a943c91ddf67b747999b72de
boudki18-art <boudki18@gmail.com>
2026-09-17 18:09:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,9 @@ const path = require('path');
 
 const app = express();
 
-// Configuration pour lire le JSON
 app.use(express.json());
 
-// Servir les fichiers statiques (CSS, JS, images)
+// C'est cette ligne magique qui permet à Express de servir index.html, styles.css et app.js tout seul
 app.use(express.static(path.join(__dirname)));
 
-// Route explicite pour la page d'accueil
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Export indispensable pour Vercel
 module.exports = app;
