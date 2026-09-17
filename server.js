a5aed4428c86594829fe50a593aadaa45f85ada4
boudki18-art <boudki18@gmail.com>
2026-09-17 18:05:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const app = express();
 // Configuration pour lire le JSON
 app.use(express.json());
 
-// Servir les fichiers statiques (HTML, CSS, JS)
+// Servir les fichiers statiques (CSS, JS, images)
 app.use(express.static(path.join(__dirname)));
 
-// Export indispensable pour Vercel (PAS de app.listen ici)
+// Route explicite pour la page d'accueil
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Export indispensable pour Vercel
 module.exports = app;
