aa5a3d61529e535df7198bbdf52dae203f3892e0
boudki18-art <boudki18@gmail.com>
2026-09-17 17:36:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-
-const requestsRouter = require('./requests');
-const authRouter = require('./auth');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-// Servir explicitement les fichiers statiques de la racine
-app.use(express.static(__dirname));
-
-// Routes API
-app.use('/api/requests', requestsRouter);
-app.use('/api/auth', authRouter);
-
-// IMPORTANT : Ne renvoyer index.html QUE pour les routes web, pas pour les fichiers manquants
+// Rediriger toutes les autres requêtes vers index.html (sauf l'API)
 app.get('*', (req, res, next) => {
   if (req.path.startsWith('/api')) {
     return next();
